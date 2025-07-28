07ca98be068724d487ae04c07e4e329a2c120b44
erwinya <chkilincer@gmail.com>
2025-07-28 23:26:01 +0300

    using security for testing

--- src/main/java/com/RestfulApi/TeacherInformationSystem/config/SecurityConfig.java
+++ src/main/java/com/RestfulApi/TeacherInformationSystem/config/SecurityConfig.java
@@ -1,5 +1,5 @@
 package com.RestfulApi.TeacherInformationSystem.config;
-/* 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,4 +28,3 @@ public class SecurityConfig {
         return http.build();
     }
 }
-*/
